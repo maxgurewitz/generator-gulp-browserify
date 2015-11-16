917621073c0d32cf74fa8bcd9401e388956b67ed
Max Gurewitz <maxthegeek1@gmail.com>
2015-11-16 14:21:21 -0800

    have npm test also run linter

--- generators/app/templates/static/gulp/tasks/lint.js
+++ generators/app/templates/static/gulp/tasks/lint.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 
 module.exports.js = {
   task: function task() {
-    return gulp.src(['src/**/*.js','test/**/*.js'])
+    return gulp.src(['src/**/*.js', 'test/**/*.js'])
       .pipe(eslint())
       .pipe(eslint.format())
       .pipe(eslint.failAfterError());
--- generators/app/templates/static/gulp/tasks/test.js
+++ generators/app/templates/static/gulp/tasks/test.js
@@ -4,7 +4,11 @@ var path = require('path');
 
 module.exports.unit = {
   task: function task() {
-    return gulp.src(path.join(__BASE, 'test', '**', '*.spec.js'), {read: false})
+    return gulp.src(path.join(__BASE, 'test', 'unit', '**', '*.spec.js'), {read: false})
       .pipe(mocha({reporter: 'spec', timeout: 15000}));
   }
+};
+
+module.exports.default = {
+  dependencies: ['lint:js', 'test:unit']
 };
