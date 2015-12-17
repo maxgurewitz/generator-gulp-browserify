a5b0677a8761ffd9efb31d4abd06d3f1e0f81c67
Max Gurewitz <maxthegeek1@gmail.com>
2015-12-16 20:49:46 -0800

    add test coverage

--- generators/app/templates/static/gulp/tasks/test.js
+++ generators/app/templates/static/gulp/tasks/test.js
@@ -1,14 +1,36 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var path = require('path');
+var istanbul = require('gulp-istanbul');
+
+function generateUnitTestStream() {
+  return gulp.src(path.join(__BASE, 'test', 'unit', '**', '*.spec.js'), {read: false})
+    .pipe(mocha({reporter: 'spec', timeout: 15000}));
+}
+
+module.exports.prepareCoverage = {
+  task: function task() {
+    return gulp.src(path.join(__BASE, 'src', '**', '*.js'))
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire());
+  }
+};
+
+module.exports.unitWithCoverage = {
+  task: function task() {
+    return generateUnitTestStream()
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+  },
+  dependencies: ['test:prepareCoverage']
+};
 
 module.exports.unit = {
   task: function task() {
-    return gulp.src(path.join(__BASE, 'test', 'unit', '**', '*.spec.js'), {read: false})
-      .pipe(mocha({reporter: 'spec', timeout: 15000}));
+    return generateUnitTestStream();
   }
 };
 
 module.exports.default = {
-  dependencies: ['lint:js', 'test:unit']
+  dependencies: ['lint:js', 'test:unitWithCoverage']
 };
